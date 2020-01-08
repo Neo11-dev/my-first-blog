4e251c6737b56e3781ca6f47fa3dd7b561ddc301
Osmar Junior <osmarlvx@hotmail.com>
2020-01-08 19:36:24 -0300

    final

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v1)m+p6_t&l-v+u4gtd4-$a__3i_$4boz9$-7uzl!d@ft2+j7m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
